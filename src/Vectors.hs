5ef6f69d297efa8fe4da3a7645913684fd2c7d9e
bardoor <dmitrybartenev58@gmail.com>
2024-09-16 21:47:30 +0300

    feat(vectors): добавил вычисление длины вектора

--- src/Vectors.hs
+++ src/Vectors.hs
@@ -1,2 +1,15 @@
 module Vectors where
 
+data Point = Point { x :: Double,
+                     y :: Double
+                   } deriving (Eq, Show)
+
+data Vector = Vector { start :: Point,
+                       end :: Point
+                     } deriving (Eq, Show)
+
+
+len :: Vector -> Double
+len (Vector {start = (Point{x = x1, y = y1}), end = (Point{x = x2, y = y2})}) 
+        = sqrt $ (x2 - x1) ** 2 + (y2 - y1) ** 2
+
